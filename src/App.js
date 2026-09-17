7c89da83aff7ea6c97fc5c64cc6ff94e86de0c2f
ds06111992-web <deepshah0246@gmail.com>
2026-09-17 21:04:12 +0530

    Add React project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import TestimonialSlider from "./components/TestimonialSlider";
+import "./App.css";
+import ZoomSlider from "./components/ZoomSlider";
+import KenBurnsSlider from "./components/KenBurnsSlider";
+import CubeSlider from "./components/CubeSlider";
+import CoverflowSlider from "./components/CoverflowSlider";
+import GalaxySlider from "./components/GalaxySlider";
+import MultipleSlider from "./components/MultipleSlider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <>
+
+            <TestimonialSlider />
+
+            <MultipleSlider />
+
+            <ZoomSlider />
+
+            <KenBurnsSlider />
+
+            <CubeSlider />
+
+            <CoverflowSlider />
+
+            <GalaxySlider />
+
+        </>
+    );
 }
 
 export default App;
